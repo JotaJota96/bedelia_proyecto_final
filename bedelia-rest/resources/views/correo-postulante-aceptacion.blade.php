5d0babf43abae88e59cdd8e88899950db452400a
JotaJota96 <jjap96@gmail.com>
2020-10-23 17:25:53 -0300

    actualizacion de reglas de negocio en Postulante

--- bedelia-rest/resources/views/correo-postulante-aceptacion.blade.php
+++ bedelia-rest/resources/views/correo-postulante-aceptacion.blade.php
@@ -1,6 +1,6 @@
 <h2>¡Hola {{ $nombre }}!</h2>
 
-<p>Le informamos que su inscripción a la carrera <b>{{ $nombreCarrera }}</b> ha cido confirmada.</p>
+<p>Le informamos que su inscripción a la carrera <b>{{ $nombreCarrera }}</b> ha sido confirmada.</p>
 
 <p>Para acceder a la plataforma con su nuevo usuario, debe dirigirse a <a href="{{ $webUrl }}">{{ $webUrl }}</a> e inicie sesión con los siguientes datos:</p>
 
