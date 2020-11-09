a05e80d82ee14843b8ac3970a89ea3213f532b30
JotaJota96 <jjap96@gmail.com>
2020-11-09 18:28:20 -0300

    mejorado cambiar contrasenia

--- bedelia-web/src/app/estudiante/cambir-contrasenia/cambir-contrasenia.component.ts
+++ bedelia-web/src/app/estudiante/cambir-contrasenia/cambir-contrasenia.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { LoginDTO } from 'src/app/clases/login-dto';
+import { LoginResponseDTO } from 'src/app/clases/login-response-dto';
 import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 @Component({
@@ -10,22 +12,23 @@ import { UsuariosService } from 'src/app/servicios/usuarios.service';
   styleUrls: ['./cambir-contrasenia.component.css']
 })
 export class CambirContraseniaComponent implements OnInit {
-  abilitas: boolean = true;
+  login: LoginResponseDTO;
+  public formulario: FormGroup;
+
   constructor(private _snackBar: MatSnackBar, protected usuService: UsuariosService, private router: Router) { }
 
-  public formulario: FormGroup;
   ngOnInit(): void {
+    this.login = this.usuService.obtenerDatosLoginAlmacenado();
 
     this.formulario = new FormGroup({
+      contraseniaActual:    new FormControl('', [Validators.required, Validators.minLength(4)]),
       contrasenia:          new FormControl('', [Validators.required, Validators.minLength(4)]),
       confirmarContrasenia: new FormControl('', [Validators.required, Validators.minLength(4)]),
     });
   }
 
   confirmar() {
-    var login = this.usuService.obtenerDatosLoginAlmacenado();
-    
-    this.usuService.passReset(login.cedula, this.formulario.controls['contrasenia'].value).subscribe(
+    this.usuService.passReset(this.login.cedula, this.formulario.controls['contrasenia'].value).subscribe(
       (datos) => {
         this.router.navigate(['/']);
       },
@@ -36,12 +39,33 @@ export class CambirContraseniaComponent implements OnInit {
   }
 
   formularioValido(){
-    if ( ! this.formulario.valid) return false;
+    if ( !this.formulario.valid) return false;
 
     if (this.formulario.controls['contrasenia'].value != this.formulario.controls['confirmarContrasenia'].value) {
       return false;
     }
     return true;
+  }
+
+  validarContraseniaActual(){
+    let ca:string = this.formulario.controls['contraseniaActual'].value;
+    let ci:string = this.login.cedula;
+    
+    if (ca.length < 4)  {
+      this.formulario.controls['contraseniaActual'].setErrors({'incorrecto': true});
+      return;
+    }
+
+
+    this.usuService.passChk(ci, ca).subscribe(
+      (datos) => {
+        this.formulario.controls['contraseniaActual'].setErrors(null);
+      },
+      (error) => {
+        this.formulario.controls['contraseniaActual'].setErrors({'incorrecto': true});
+      }
+    );
+
   }
 
   openSnackBar(mensaje: string) {
