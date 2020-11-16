a0fec8d595bd74538171dd47538b8e6e9faff6f6
JotaJota96 <jjap96@gmail.com>
2020-11-15 23:18:05 -0300

    mejora color de snackbar

--- bedelia-web/src/app/global-functions.ts
+++ bedelia-web/src/app/global-functions.ts
@@ -4,10 +4,24 @@
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-export function openSnackBar(_snackBar: MatSnackBar, mensaje : string) {
-  _snackBar.open(mensaje,'Salir', {
-    duration: 5000,
+export function openSnackBar(_snackBar: MatSnackBar, mensaje : string, tipo : 'ok'|'error'= 'ok') {
+  let clasesCss: string[] = [];
+  switch (tipo) {
+    case 'ok':
+      clasesCss = ['mat-snack-bar-container-error'];
+      break;
+    case 'error':
+      clasesCss = ['mat-snack-bar-container-ok'];
+      break;
+  }
+
+  _snackBar.open(mensaje,'Cerrar', {
+    duration: 4000,
+    panelClass: clasesCss,
     horizontalPosition: 'center',
     verticalPosition: "top",
   });
+}
+export function test(){
+  openSnackBar(null, "", "error");
 }
